60092850f8c94a9cbeae521cce495a81499faf26
dlcn <microccl@foxmail.com>
2023-03-16 17:36:45 +0800

    http blocking connect

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,1 @@
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://baidu.com").await?.text().await?;
-    println!("{:#?}", resp);
-    Ok(())
-}
+fn main() {}
--- tests/http_test.rs
+++ tests/http_test.rs
@@ -1,8 +1,16 @@
 #[cfg(test)]
 mod http {
     #[tokio::test]
-    async fn test_connect() {
+    async fn async_connect() {
         let code = reqwest::get("https://baidu.com").await.unwrap().status();
+        assert_eq!(200, code);
+    }
+
+    #[test]
+    fn blocking_connect() {
+        let code = reqwest::blocking::get("https://baidu.com")
+            .unwrap()
+            .status();
         assert_eq!(200, code);
     }
 }
